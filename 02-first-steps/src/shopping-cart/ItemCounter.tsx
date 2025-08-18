732180f5b36fe1a1931bd6a0c79a75131c322905
Guerrero Welinton <guerrerozamora213@gmail.com>
2025-08-18 00:40:26 -0500

    fix: restructuring of folders and completion of section 2

---
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+// import './ItemCounter.css';
+import styles from './ItemCounter.module.css';
+
+interface Props {
+  productName: string;
+  quantity?: number;
+}
+
+export const ItemCounter = ({ productName, quantity = 1 }: Props) => {
+  const [count, setCount] = useState<number>(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    if (count > 0) setCount(count - 1);
+  };
+
+  return (
+    <section className={styles.itemRow}>
+      <span className={styles['item-text']} style={{ color: count > 0 ? 'green' : 'black' }}>
+        {productName}
+      </span>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button onClick={handleSubtract}>-1</button>
+    </section>
+  );
+};
+
+// No usar las funciones dispachers (set) del useState directamente el componente
+// ya que, esta función provoca un re-render del componente y genera un bucle infinito
+
+// Orden de las importaciones
+// React
+// Librerias de terceros
+// Archivos personalizados
+// Css
+
+// Los css module debe llevar el .module
