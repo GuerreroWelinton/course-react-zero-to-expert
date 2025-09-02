5fc6cafb368590b887ee1610d26c945641950b0b
Guerrero Welinton <guerrerozamora213@gmail.com>
2025-09-02 00:51:23 -0500

    video - section 5, chapter 14 - mock components

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,11 +1,23 @@
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
 import { FirstStepsApp } from './FirstStepsApp';
+
+vi.mock('./shopping-cart/ItemCounter', () => ({
+  ItemCounter: () => <div data-testid="ItemCounter"></div>,
+}));
 
 describe('FirstStepsApp', () => {
   test('should match snapshot', () => {
     const { container } = render(<FirstStepsApp />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('should render the correct number of ItemCounter components', () => {
+    render(<FirstStepsApp />);
+
+    const itemCounterComponents = screen.getAllByTestId('ItemCounter');
+
+    expect(itemCounterComponents.length).toBe(4);
   });
 });
