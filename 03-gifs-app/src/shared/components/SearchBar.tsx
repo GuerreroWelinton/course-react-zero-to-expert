716603b2329e05dd4b13e851c8fedf15fa1ff49e
Guerrero Welinton <guerrerozamora213@gmail.com>
2025-09-14 23:57:46 -0500

    video - section 6, chapter 10 - useEffect - Debounce

--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState, type KeyboardEvent } from 'react';
+import { useEffect, useState, type KeyboardEvent } from 'react';
 
 interface Props {
   placeholder?: string;
@@ -8,8 +8,19 @@ interface Props {
 export const SearchBar = ({ placeholder = 'Buscar', onQuery }: Props) => {
   const [query, setQuery] = useState('');
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onQuery(query);
+    }, 700);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  });
+
   const handleSearch = () => {
     onQuery(query);
+    // setQuery('');
   };
 
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
