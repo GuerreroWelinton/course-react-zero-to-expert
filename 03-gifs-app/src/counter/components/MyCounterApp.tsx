4f23ea412e4c2bf0a3f2f208be694c105177dea6
Guerrero Welinton <guerrerozamora213@gmail.com>
2025-09-20 00:02:21 -0500

    video - section 7, chapter 6 - custom hooks

--- 03-gifs-app/src/counter/components/MyCounterApp.tsx
+++ 03-gifs-app/src/counter/components/MyCounterApp.tsx
@@ -1,19 +1,7 @@
-import { useState } from 'react';
+import { useCounter } from '../hooks/useCounter';
 
 export const MyCounterApp = () => {
-  const [counter, setCounter] = useState<number>(5);
-
-  const handleAdd = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleSubtract = () => {
-    setCounter(prevState => prevState - 1);
-  };
-
-  const handleReset = () => {
-    setCounter(5);
-  };
+  const { counter, handleAdd, handleSubtract, handleReset } = useCounter(5);
 
   return (
     <div
